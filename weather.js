5286688a296ff40850c995c2f727b7c18d2f4b6d
Marcia <marciaadler718@gmail.com>
2020-01-16 14:29:53 -0500

    updated styling, add UV index, and add API for 5 day forecast

--- weather.js
+++ weather.js
@@ -1,31 +1,13 @@
 $(document).ready(function(){
     
-    var searchForecast = $('<div>').addClass('forecast');
+    var searchForecast = $('<div>').addClass('forecast card-body');
     $('.city-forecast').append(searchForecast);
 
     // searchForecast.addClass('search-forecast card ');
     // $('.city-forecast').append(searchForecast);
 
 
-    var forecastDayOne = $('<div>');
-    forecastDayOne.addClass('card-body text-white bg-info mb-3 day-one');
-    var forecastDayTwo = $('<div>');
-    forecastDayTwo.addClass('card-body text-white bg-info mb-3 day-two');
-    var forecastDayThree = $('<div>');
-    forecastDayThree.addClass('card-body text-white bg-info mb-3 day-three');
-    var forecastDayFour = $('<div>');
-    forecastDayFour.addClass('card-body text-white bg-info mb-3 day-four');
-    var forecastDayFive = $('<div>');
-    forecastDayFive.addClass('card-body text-white bg-info mb-3 day-five');
-
-    var fiveDayForecast = $('<div>');
-    fiveDayForecast.addClass('five-day');
-    $('.day-one').append(forecastDayOne);
-    $('.day-two').append(forecastDayTwo);
-    $('.day-three').append(forecastDayThree);
-    $('.day-four').append(forecastDayFour);
-    $('.day-five').append(forecastDayFive);
-    $('.five-day-forecast').append(fiveDayForecast);
+    
 
     
     $('#search').click(function(){
@@ -40,6 +22,8 @@ $(document).ready(function(){
         localStorage.setItem('key',JSON.stringify(cities))
         populateSearchHistory() 
         searchByCity(city.val());
+        populateFiveDay(city.val())
+
         $('#input').val('');
             
     })
@@ -58,7 +42,7 @@ $(document).ready(function(){
     populateSearchHistory()
 
     function searchByCity(city){
-        var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=4074b5f07760c717f95e1eec3f81ef1f";
+        var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=4074b5f07760c717f95e1eec3f81ef1f";
         $.ajax({
             url:queryURL,
             method: "GET"
@@ -68,18 +52,65 @@ $(document).ready(function(){
             var humidity = $('<p>').addClass('attribute').text(`Humidity: ${response.main.humidity}%`);
             var windSpeed = $('<p>').addClass('attribute').text(`Wind Speed ${response.wind.speed} MPH`);  
             var name = $('<h2>').addClass('city').text(response.name);
-            name.append(temperature);
-            name.append(humidity);
-            name.append(windSpeed);
             searchForecast.append(name);
+            searchForecast.append(temperature);
+            searchForecast.append(humidity);
+            searchForecast.append(windSpeed);
+            //grab latitude to use in UV index
+            // grab longitude to use in UV index
+            var lat = (response.coord.lat);
+            var lon = (response.coord.lon);
+            console.log(lat,lon);
             
             
-        })
+            
+            // add date next to name
+            // add UV Index to card
+            getUVIndex(lat,lon);
+            
+            
         
-    }
-
+        })
+        function getUVIndex(lat,lon){
+        var queryURL2= "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=4074b5f07760c717f95e1eec3f81ef1f";
+            $.ajax({
+                url:queryURL2,
+                method: "GET"
+            }).then(function(response){
+                console.log(response);
+                var uvIndex = $('<p>').addClass('attribute').text(`UV Index ${response[0].value}`);
+                searchForecast.append(uvIndex);
+                console.log(response[0].value)
+            }) 
+        }    
+    }            
     // create function to populate five day forecast.  use .each to loop through days.
+    function populateFiveDay(city){
+        var queryURL= "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&mode=xml&appid=4074b5f07760c717f95e1eec3f81ef1f";
+        $.ajax({
+            url:queryURL,
+            method: "GET"
+        }).then(function(response){
+            console.log(response);
+            var forecastDayOne = $('<div>').addClass('card-body text-white bg-info mb-3 day-one');
+            var forecastDayTwo = $('<div>').addClass('card-body text-white bg-info mb-3 day-two');
+            var forecastDayThree = $('<div>').addClass('card-body text-white bg-info mb-3 day-three');;
+            var forecastDayFour = $('<div>').addClass('card-body text-white bg-info mb-3 day-four');;
+            var forecastDayFive = $('<div>').addClass('card-body text-white bg-info mb-3 day-five');;
+
+            var fiveDayForecast = $('<div>').addClass('five-day');
+            
+            (fiveDayForecast).append(forecastDayOne);
+            (fiveDayForecast).append(forecastDayTwo);
+            (fiveDayForecast).append(forecastDayThree);
+            (fiveDayForecast).append(forecastDayFour);
+            (fiveDayForecast).append(forecastDayFive);
+            $('.five-day-forecast').append(fiveDayForecast);
+        })
 
     // add event listener to search by city to be able to click on buttons in search history
+        }
+    
 })
+   
 
